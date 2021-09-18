99116f8c10d24b6ac09adb196746c8984715430d
Shiro39 <38461122+shirooo39@users.noreply.github.com>
2021-09-18 15:45:47 +0700

    Update index.js
    Added support for service account.

--- index.js
+++ index.js
@@ -2,21 +2,50 @@
 //
 // Main Configurations
 //
+
+//
+// Service Account
+//
+const serviceaccounts = [
+  // Your service account(s) goes here.
+  // Multiple service accounts are also supported.
+  //
+  /*****
+  {
+    "type": "service_account",
+    "project_id": "BLANK",
+    "private_key_id": "BLANK",
+    "private_key": "BLANK",
+    "client_email": "BLANK",
+    "client_id": "BLANK",
+    "auth_uri": "BLANK",
+    "token_uri": "BLANK",
+    "auth_provider_x509_cert_url": "BLANK",
+    "client_x509_cert_url": "BLANK"
+  }
+  *****/
+];
+const randomserviceaccount = serviceaccounts[Math.floor(Math.random()*serviceaccounts.length)]; // Do NOT modify this!
+
+//
+// Authentication Configurations
+//
 const authConfig = {
   "siteName": "Site Title",  // Site title. Set this to whatever you want and this will show up on the browser's tab.
   "siteIcon": "https://cdn.jsdelivr.net/gh/shirooo39/GDIndex@5mayrain/dist/img/favicon.ico", // The favicon. Can be whatever you want.
   // "version": "2.0.5", // Script version. Safe to edit.
 
-  /**************************************************
-  "client_id": "202264815644.apps.googleusercontent.com",
-  "client_secret": "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
-  **************************************************/
   // The rclone documentation recommended to use your own client_id.
   "client_id": "",
   "client_secret": "",
   "refresh_token": "",
   // You can get the refresh_token from your rclone.conf file
   // (assuming you already have rclone set up and working).
+
+  "service_account": false, // Set this to "true" if you want to use Service Account.
+	"service_account_json": randomserviceaccount, // Do NOT modify this one!
+	// Make sure you already have your SAs added into the target drive first! (be it as a Google Group or one-by-one)
+	// If you enable the service_account, you can leave the refresh_token blank.
 
   /**************************************************
    * 
@@ -44,24 +73,24 @@ const authConfig = {
     * or you can just give them both values.
     **************************************************/
     {
-      id: "id_1", // Fill this with a drive id (personal or team drive) or a folder id.
+      id: "root_id_1", // Fill this with a drive id (personal or team drive) or a folder id.
       name: "Name 1",
-      user: '',
+      user: "",
       pass: "",
       protect_file_link: false // Set this to "true" to enable the Basic HTTP Authentication.
     }
     /**************************************************
     {
-      id: "id_2", // Fill this with a drive id (personal or team drive) or a folder id.
+      id: "root_id_2", // Fill this with a drive id (personal or team drive) or a folder id.
       name: "Name 2",
-      user: '',
+      user: "",
       pass: "",
       protect_file_link: false // Set this to "true" to enable the Basic HTTP Authentication.
     },
     {
-      id: "id_3", // Fill this with a drive id (personal or team drive) or a folder id.
+      id: "root_id_3", // Fill this with a drive id (personal or team drive) or a folder id.
       name: "Name 3",
-      user: '',
+      user: "",
       pass: "",
       protect_file_link: false // Set this to "true" to enable the Basic HTTP Authentication.
     }
@@ -221,6 +250,67 @@ function html(current_drive_order = 0, model = {}) {
 </body>
 </html>
 `;
+};
+
+const JSONWebToken = {
+  header: {
+      alg: 'RS256',
+      typ: 'JWT'
+  },
+  importKey: async function(pemKey) {
+      var pemDER = this.textUtils.base64ToArrayBuffer(pemKey.split('\n').map(s => s.trim()).filter(l => l.length && !l.startsWith('---')).join(''));
+      return crypto.subtle.importKey('pkcs8', pemDER, {
+          name: 'RSASSA-PKCS1-v1_5',
+          hash: 'SHA-256'
+      }, false, ['sign']);
+  },
+  createSignature: async function(text, key) {
+      const textBuffer = this.textUtils.stringToArrayBuffer(text);
+      return crypto.subtle.sign('RSASSA-PKCS1-v1_5', key, textBuffer)
+  },
+  generateGCPToken: async function(serviceAccount) {
+      const iat = parseInt(Date.now() / 1000);
+      var payload = {
+          "iss": serviceAccount.client_email,
+          "scope": "https://www.googleapis.com/auth/drive",
+          "aud": "https://oauth2.googleapis.com/token",
+          "exp": iat + 3600,
+          "iat": iat
+      };
+      const encPayload = btoa(JSON.stringify(payload));
+      const encHeader = btoa(JSON.stringify(this.header));
+      var key = await this.importKey(serviceAccount.private_key);
+      var signed = await this.createSignature(encHeader + "." + encPayload, key);
+      return encHeader + "." + encPayload + "." + this.textUtils.arrayBufferToBase64(signed).replace(/\//g, '_').replace(/\+/g, '-');
+  },
+  textUtils: {
+      base64ToArrayBuffer: function(base64) {
+          var binary_string = atob(base64);
+          var len = binary_string.length;
+          var bytes = new Uint8Array(len);
+          for (var i = 0; i < len; i++) {
+              bytes[i] = binary_string.charCodeAt(i);
+          }
+          return bytes.buffer;
+      },
+      stringToArrayBuffer: function(str) {
+          var len = str.length;
+          var bytes = new Uint8Array(len);
+          for (var i = 0; i < len; i++) {
+              bytes[i] = str.charCodeAt(i);
+          }
+          return bytes.buffer;
+      },
+      arrayBufferToBase64: function(buffer) {
+          let binary = '';
+          let bytes = new Uint8Array(buffer);
+          let len = bytes.byteLength;
+          for (let i = 0; i < len; i++) {
+              binary += String.fromCharCode(bytes[i]);
+          }
+          return btoa(binary);
+      }
+  }
 };
 
 addEventListener('fetch', event => {
@@ -870,13 +960,22 @@ class googleDrive {
     console.log("fetchAccessToken");
     const url = "https://www.googleapis.com/oauth2/v4/token";
     const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
     };
-    const post_data = {
-      'client_id': this.authConfig.client_id,
-      'client_secret': this.authConfig.client_secret,
-      'refresh_token': this.authConfig.refresh_token,
-      'grant_type': 'refresh_token'
+    var post_data;
+    if (this.authConfig.service_account && typeof this.authConfig.service_account_json != "undefined") {
+        const jwttoken = await JSONWebToken.generateGCPToken(this.authConfig.service_account_json);
+        post_data = {
+            grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            assertion: jwttoken,
+        };
+    } else {
+        post_data = {
+            client_id: this.authConfig.client_id,
+            client_secret: this.authConfig.client_secret,
+            refresh_token: this.authConfig.refresh_token,
+            grant_type: "refresh_token",
+        };
     }
 
     let requestOption = {
